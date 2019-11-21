34869bf0738968553a8315864e6ea96240bdb2a6
segoviawalter <naka26@hotmail.com>
2019-11-21 00:06:37 -0300

    Add files via upload
    SP-19-11-2019

---
+++ LabII-SP2019/Apellido.Nombre.Div/SistemaSolar/FormSistemaSolar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SistemaSolar
+{
+    public partial class FormSistemaSolar : Form
+    {
+        public FormSistemaSolar()
+        {
+                InitializeComponent();
+        }
+
+        private void btnRotarPlaneta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOrbitarSatelite_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSatelite_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
